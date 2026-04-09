c44c04deb336b51492b0b175439ecf867f1e03af
Ilyass Afriad <ilyassafriad@gmail.com>
2026-04-09 14:05:06 +0100

    add logging & schedule utils functions

--- backup_manager.py
+++ backup_manager.py
@@ -1,7 +1,27 @@
 import os
+from datetime import datetime
+
 
 SCHEDULE_FILE = "backup_schedules.txt"
 LOG_DIR = "logs"
 LOG_FILE = os.path.join(LOG_DIR, "backup_manager.log")
 BACKUP_DIR = "backups"
 PID_FILE = "backup_service.pid"
+
+
+# ---------------- Logging ---------------- #
+def log(message):
+    os.makedirs(LOG_DIR, exist_ok=True)
+    timestamp = datetime.now().strftime("[%d/%m/%Y %H:%M]")
+    with open(LOG_FILE, "a") as f:
+        f.write(f"{timestamp} {message}\n")
+
+
+# ---------------- Schedule Utils ---------------- #
+def validate_schedule(schedule):
+    try:
+        path, time_str, name = schedule.split(";")
+        datetime.strptime(time_str, "%H:%M")
+        return path and name
+    except:
+        return False
